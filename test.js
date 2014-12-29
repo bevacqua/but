602f6919dae71364af80ecc05822dd93e0fa3c1f
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-12-29 16:52:42 -0300

    return results as well

--- test.js
+++ test.js
@@ -2,7 +2,12 @@
 
 var but = require('./');
 var n = [1, 2, 3, 4, 5];
+var m = ['1a', '2a', '3a', '4a', '5a'];
 
 n.forEach(console.log);
 
 n.forEach(but(console.log));
+
+m.map(parseInt).forEach(but(console.log));
+
+m.map(but(parseInt)).forEach(but(console.log));
